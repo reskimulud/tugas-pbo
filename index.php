7e80fc69983e954cfe9f023e391948c119a44c57
Reski Mulud Muchamad <63949402+reski-mulud-muchamad@users.noreply.github.com>
2021-10-27 21:53:40 +0700

    add update name

--- index.php
+++ index.php
@@ -1,35 +1,65 @@
 <?php
-class user{
+class User{
     public $name;
     public $email;
     public $birthYear;
+    public $address;
 
-    public function __construct($name, $email) {
+    protected function __construct($name, $email) {
         $this->name = $name;
         $this->email = $email;
     }
 
-    protected function getname() {
-        echo $this->name;
+    protected function setName($newName) {
+        if ($newName != $this->name) {
+            $this->name = $newName;
+        } else {
+            throw new Exception("Name tidak boleh sama", 1);
+        }
+    }
+
+    public function getName() {
+        return $this->name;
     }
 
     public function getAge() {
         return date('Y') - $this->birthYear;
     }
-}
 
-class member extends user {
-    
-    
-    public function {
-        
+    public function getProperties() {
+        return "Nama : $this->name, Email : $this->email";
     }
 }
 
-class admin extends user {
+class Member extends User {
+
+    public $userId;
+
+    public function __construct($name, $email) {
+        parent::__construct($name, $email);
+        $this->userId = 'M' . time();
+    }
+
+    public function updateName($newName) {
+        try {
+            parent::setName($newName);
+        } catch (Exception $e) {
+            echo "Error : " . $e;
+        }
+    }
+
+}
+
+class Admin extends User {
 
 
 }
-?>
 
-# comand
+$reski = new Member("Reski Mulud", "reski.mulud@gmail.com");
+echo $reski->getProperties();
+
+#update name
+$reski->updateName("Reski Mulud");
+echo $reski->name; // error karena nama awal sama dengan nama yang baru
+
+?>
