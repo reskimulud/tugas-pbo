56fb3ee8d476159f1583e080c8cdd60472dc3214
BanggaA <93152023+BanggaA@users.noreply.github.com>
2021-10-27 20:21:11 +0700

    add name and email

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<?php
+class user{
+    public $name;
+    public $email;
+
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getname() {
+        echo $this->name;
+    }
+}
+
+
+
+?>
+
 # comand
