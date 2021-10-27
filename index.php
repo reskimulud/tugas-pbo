71173e8b0fc952f60a6d1edde8ae8e6688c4f7aa
Reski Mulud Muchamad <63949402+reski-mulud-muchamad@users.noreply.github.com>
2021-10-27 20:26:21 +0700

    add birthYear and method age

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 class user{
     public $name;
     public $email;
+    public $birthYear;
 
     public function __construct($name, $email) {
         $this->name = $name;
@@ -10,6 +11,10 @@ class user{
 
     public function getname() {
         echo $this->name;
+    }
+
+    public function getAge() {
+        return date('Y') - $this.$birthYear;
     }
 }
 
