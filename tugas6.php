c49c13eefb1402723193cbf7983131ae735a8eb3
Reski Mulud Muchamad <63949402+reski-mulud-muchamad@users.noreply.github.com>
2021-12-16 10:20:43 +0700

    tugas 6 - error handling

---
+++ tugas6.php
@@ -0,0 +1,86 @@
+<?php
+class User
+{
+    public $name;
+    public $email;
+    public $birthYear;
+    public $address;
+
+    protected function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    protected function setName($newName)
+    {
+        if ($newName != $this->name) {
+            $this->name = $newName;
+        } else {
+            // lemparkan error jika nama pertama sama yang baru sama
+            throw new Exception("Name tidak boleh sama");
+        }
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return date('Y') - $this->birthYear;
+    }
+
+    public function __destruct(){
+        echo "Nama : $this->name, Email : $this->email";
+    }
+}
+
+class Member extends User
+{
+
+    private $memberId;
+
+    public function __construct($name, $email)
+    {
+        parent::__construct($name, $email);
+        $this->memberId = 'M' . time();
+    }
+
+    public function updateName($newName)
+    {
+        /**
+         * Implementasi try catch
+         */
+        try {
+            parent::setName($newName);
+        } catch (Exception $e) {
+            echo "Error : " . $e;
+        }
+    }
+}
+
+class Admin extends User
+{
+
+    private $adminId;
+
+    public function __construct($name, $email)
+    {
+        parent::__construct($name, $email);
+        $this->adminId = 'A' . time();
+    }
+}
+
+$deri = new Admin('Deri Kurniawan', 'deri.netuchi@gmail.com');
+$deri;
+
+$reski = new Member("Reski Mulud", "reski.mulud@gmail.com");
+$reski;
+
+#update name
+$reski->updateName("Reski Mulud");
+echo $reski->name; // error karena nama awal sama dengan nama yang baru
+
+?>
